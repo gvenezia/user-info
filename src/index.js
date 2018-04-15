de03a2fd25de754b1fe78a2e2d4ee42b3b850494
gvenezia <g.venezia@outlook.com>
2018-04-15 21:05:17 +0000

    Separate 'greeting' into a separate component

--- src/index.js
+++ src/index.js
@@ -15,19 +15,19 @@ class UserInfo extends React.Component {
             friendsList: ['J T', 'X A', 'C T']
         }
         
-        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
     
-    handleChange(e){
+    handleClick(e){
         this.setState({
             friendsList: this.state.friendsList.push(e.target.value),
-        })
+        });
     }
     
     render() {
         return(
             <div>
-                Hello, {this.state.name}!
+                <Greeting name={this.state.name}/>
                 <br/>
                 <FriendsContainer 
                     name={this.state.name}
@@ -45,28 +45,26 @@ class FriendsContainer extends React.Component {
             <div>Add a friend name:
                 <input
                     type='text'
-                    value={this.props.name}
-                    onChange={this.handleChange}
                 />
                 <button 
                     type='submit'
+                    onClick={this.props.handleClick}
                 > Add Friend!
                 </button>
                 <br/>
                 <h3>Friends</h3>
                 <ul>
                     {this.props.friendsList.map((currName) => {<li>{currName}</li>})}
-                    
-                    <Friend
-                        // key={}
-                        friendsList={this.props.friendsList}
-                    />
                 </ul>
                 
             </div>
         );
     }
-} // End FriendsList
+} // End FriendsContainer
+
+function Greeting(props){
+    return <div>Hello, {props.name}!</div> ;
+}
 
 function Friend(props) {
     return(
