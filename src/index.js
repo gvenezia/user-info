9162659e245ed67931bfc27b14ac614e2ed5f411
gvenezia <g.venezia@outlook.com>
2018-04-15 20:40:12 +0000

    Add state and components for a list of friends

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ class UserInfo extends React.Component {
         
         this.state = {
             name: 'Gae',
-            
+            friendsList: ['J T', 'X A', 'C T']
         }
         
         this.handleChange = this.handleChange.bind(this);
@@ -20,28 +20,59 @@ class UserInfo extends React.Component {
     
     handleChange(e){
         this.setState({
-            name: e.target.value,
+            friendsList: this.state.friendsList.push(e.target.value),
         })
     }
     
     render() {
         return(
             <div>
-            Hello, {this.state.name}
-            <br/>
-            Change name:
-                <input
-                    type='text'
-                    value={this.state.name}
-                    onChange={this.handleChange}
+                Hello, {this.state.name}!
+                <br/>
+                <FriendsContainer 
+                    name={this.state.name}
+                    friendsList={this.state.friendsList}
                 />
             </div>
         );
   
     };
-    
-      
-}
+} // End UserInfo
+
+class FriendsContainer extends React.Component {
+    render() {
+        return (
+            <div>Add a friend name:
+                <input
+                    type='text'
+                    value={this.props.name}
+                    onChange={this.handleChange}
+                />
+                <button 
+                    type='submit'
+                > Add Friend!
+                </button>
+                <br/>
+                <h3>Friends</h3>
+                <ul>
+                    {this.props.friendsList.map((currName) => {<li>{currName}</li>})}
+                    
+                    <Friend
+                        // key={}
+                        friendsList={this.props.friendsList}
+                    />
+                </ul>
+                
+            </div>
+        );
+    }
+} // End FriendsList
+
+function Friend(props) {
+    return(
+        <li></li>
+    );
+};
 
 // =============== React DOM Render =========================
 
